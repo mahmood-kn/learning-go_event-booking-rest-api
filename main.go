1cd02fed697bb82459fb5c2a50b4f57e417482c4
Mahmood <mahmood.kn1526@gmail.com>
2024-03-22 15:48:37 +0330

    add save event & api test dir

--- main.go
+++ main.go
@@ -24,10 +24,13 @@ func createEvent(context *gin.Context) {
 	var event models.Event
 	err := context.ShouldBindJSON(&event)
 	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data"})
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data", "error": err})
 		return
 	}
 	event.ID = 1
 	event.UserID = 1
+
+	event.Save()
+
 	context.JSON(http.StatusCreated, gin.H{"message": "Event created", "event": event})
 }
