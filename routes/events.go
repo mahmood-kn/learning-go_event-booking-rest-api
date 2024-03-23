41d0b48bd2cb467a111c8be527b7edc8cd4de6af
Mahmood <mahmood.kn1526@gmail.com>
2024-03-23 18:27:32 +0330

    work on login

--- routes/events.go
+++ routes/events.go
@@ -5,6 +5,7 @@ import (
 	"strconv"
 
 	"exmaple.com/event-booking-rest-api/models"
+	"exmaple.com/event-booking-rest-api/utils"
 	"github.com/gin-gonic/gin"
 )
 
@@ -38,13 +39,20 @@ func createEvent(context *gin.Context) {
 		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not Authorized"})
 		return
 	}
+
+	err := utils.VerifyToken(token)
+
+	if err != nil {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not Authorized"})
+		return
+	}
+
 	var event models.Event
-	err := context.ShouldBindJSON(&event)
+	err = context.ShouldBindJSON(&event)
 	if err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data", "error": err})
 		return
 	}
-	event.ID = 1
 	event.UserID = 1
 
 	err = event.Save()
