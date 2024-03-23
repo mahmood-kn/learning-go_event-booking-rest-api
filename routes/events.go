8ecbe2d34a7e8729d1016c153ca4be8726e1fe3f
Mahmood <mahmood.kn1526@gmail.com>
2024-03-23 18:50:38 +0330

    add middleware to protect routes

--- routes/events.go
+++ routes/events.go
@@ -5,7 +5,6 @@ import (
 	"strconv"
 
 	"exmaple.com/event-booking-rest-api/models"
-	"exmaple.com/event-booking-rest-api/utils"
 	"github.com/gin-gonic/gin"
 )
 
@@ -34,25 +33,14 @@ func getEvent(context *gin.Context) {
 }
 
 func createEvent(context *gin.Context) {
-	token := context.Request.Header.Get("Authorization")
-	if token == "" {
-		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not Authorized"})
-		return
-	}
-
-	userId, err := utils.VerifyToken(token)
-
-	if err != nil {
-		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not Authorized"})
-		return
-	}
 
 	var event models.Event
-	err = context.ShouldBindJSON(&event)
+	err := context.ShouldBindJSON(&event)
 	if err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data", "error": err})
 		return
 	}
+	userId := context.GetInt64("userId")
 	event.UserID = userId
 
 	err = event.Save()
