a0c72e54c5667c4ad45fbccdf2dd0892ccb1f329
Mahmood <mahmood.kn1526@gmail.com>
2024-03-23 15:25:01 +0330

    working on login

--- models/user.go
+++ models/user.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"errors"
+
 	"exmaple.com/event-booking-rest-api/db"
 	"exmaple.com/event-booking-rest-api/utils"
 )
@@ -31,4 +33,20 @@ func (u *User) Save() error {
 	u.ID = userId
 	return err
 
+}
+
+func (u *User) ValidateCredentials() error {
+	query := "SELECT password FROM users WHERE email = ?"
+	row := db.DB.QueryRow(query, u.Email)
+	var retrievedPassword string
+	err := row.Scan(&retrievedPassword)
+	if err != nil {
+		return errors.New("Credentials invalid")
+	}
+	passwordIsValid := utils.CheckPasswordHash(u.Password, retrievedPassword)
+
+	if !passwordIsValid {
+		return errors.New("Credentials invalid")
+	}
+	return nil
 }
