fd3382b77d11e427efe7da2fc2c1e65943bb640a
Mahmood <mahmood.kn1526@gmail.com>
2024-03-23 19:23:13 +0330

    add registration route and functionality

---
+++ routes/register.go
@@ -0,0 +1,33 @@
+package routes
+
+import (
+	"net/http"
+	"strconv"
+
+	"exmaple.com/event-booking-rest-api/models"
+	"github.com/gin-gonic/gin"
+)
+
+func registerForEvent(context *gin.Context) {
+	userId := context.GetInt64("userId")
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event."})
+		return
+
+	}
+	event, err := models.GetEventByID(eventId)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "could not fetch event"})
+		return
+	}
+	err = event.Register(userId)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "could not register user for event"})
+		return
+	}
+	context.JSON(http.StatusCreated, gin.H{"message": "registered!"})
+
+}
+
+func cancelRegistrationEvent(context *gin.Context) {}
