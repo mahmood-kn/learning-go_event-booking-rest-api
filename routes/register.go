6f66fb543a2d901172e5f9ccd5a3ab2d8dc327fd
Mahmood <mahmood.kn1526@gmail.com>
2024-03-23 19:34:50 +0330

    add cancel registration

--- routes/register.go
+++ routes/register.go
@@ -30,4 +30,20 @@ func registerForEvent(context *gin.Context) {
 
 }
 
-func cancelRegistrationEvent(context *gin.Context) {}
+func cancelRegistrationEvent(context *gin.Context) {
+	userId := context.GetInt64("userId")
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event."})
+		return
+
+	}
+	var event models.Event
+	event.ID = eventId
+	err = event.CancelRegistration(userId)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "could not cancel registration "})
+		return
+	}
+	context.JSON(http.StatusOK, gin.H{"message": "canceled!"})
+}
